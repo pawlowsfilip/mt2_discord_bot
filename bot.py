d110584760208e457f276fcddbca80f09b935e78
Filip Pawlowski <pawlows.filip@gmail.com>
2023-04-06 11:36:21 +0200

    Moved all functions from responses to bot file

--- bot.py
+++ bot.py
@@ -1,29 +1,56 @@
 import discord
-import responses
-
-
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+from discord.ext import commands
+from config import TOKEN
+from statue import statue
 
 
 def run_discord_bot():
-    TOKEN = 'MTA5MjgxOTM0ODgxODc3MjAwMA.GNTSvb.l1IzTGOHg0MXrirTGptmzp-h0_KclaXuuHUF1o'
     client = discord.Client()
+    bot = commands.Bot(command_prefix="!")
 
     @client.event
     async def on_ready():
-        print(f'{client.user} is no running!')
+        print(f'Logged as {client.user}')
+
+    @bot.event
+    async def on_member_join(member):
+        channel = discord.utils.get(member.guild.channels, name='『📋』regulamin')
+        if channel:
+            embed = discord.Embed(title='Regulamin', description=statue, color=0x00ff00, )
+            await channel.send(embed=embed)
+            message = await channel.history().get(author=client.user)
+            await message.add_reaction('✅')
+
+    @client.event
+    async def on_raw_reaction_add(payload):
+        channel_id = '『📋』regulamin'
+        if payload.channel_id == channel_id:
+            return
+
+        guild = client.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+
+        if payload.emoji.name == '✅':
+            role = discord.utils.get(guild.roles, name='Zweryfikowany')
+            await payload.member.add_roles(role)
+
+    @client.event
+    async def on_message(message):
+        username = str(message.author).split("#")[0]
+        channel = str(message.channel.name)
+        user_message = str(message.content)
+
+        print(f'Message {user_message} by {username} on {channel}')
+
+        if message.author == client.user:
+            return
+
+        if user_message.lower() == "hello" or user_message.lower() == "hi":
+            await message.channel.send(f'Hello {username}')
+            return
+
+        # Respond to commands
+        if message.content.startswith("!roles"):
+            await on_member_join(message.author)
 
     client.run(TOKEN)
-
-
-def handle_response(message) -> str:
-    p_message = message.lower()
-
-    if p_message == 'hello':
-        return 'hello there!'
-
