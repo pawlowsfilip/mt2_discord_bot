e39d7c6387f1bd218d424746d749e99b479121a2
Filip Pawlowski <pawlows.filip@gmail.com>
2023-04-06 11:02:08 +0200

    Created statue.py file with variable statue for better visibility

---
+++ statue.py
@@ -0,0 +1,18 @@
+statue = '''§1. Każdy użytkownik przebywający na naszym discordzie akceptuje jego regulamin.
+
+§2. Nieznajomość regulaminu nie zwalnia z obowiązku przestrzegania go.
+
+§3. Należy również przestrzegać Warunków korzystania z usługi Discord.
+
+§4. Odnosimy się do siebie z szacunkiem oraz kulturą.
+
+§5. Do czynności zabronionych należy m.in:
+- upublicznianie informacji pofunych, danych osobowych (imion,nazwisk,adresów,numerów) 
+  innych użytkowników
+- podszywanie sie pod inne osoby, w szczególności administracje
+- nagrywanie rozmów, bez zgody rozmówców
+- trollowanie
+- obraza innych uzytkowników, celowa prowokacja, groźby
+
+§6. Administracja ma prawo ukarać użytkownika bez podania powodu, gdy uzna że użytkownik
+    działa na szkodę serwera.'''
