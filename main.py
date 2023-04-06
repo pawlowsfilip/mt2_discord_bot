e7984687a6b17740b5ce381fa9d97953ca46dda6
Filip Pawlowski <pawlows.filip@gmail.com>
2023-04-06 10:54:08 +0200

    Updated main.py file for better work organizing

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import bot
+import discord
+from discord.ext import commands
+from config import TOKEN
 
 if __name__ == '__main__':
-    bot.run_discord_bot()
+    # Create bot instance
+    bot_client = commands.Bot(command_prefix="!")
+
+    # Initialize bot
+    bot.initialize(bot_client)
+
+    # Run the bot
+    bot_client.run(TOKEN)
