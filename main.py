4d88a5ee24932026f9a134a5343d854fb547222c
Filip Pawlowski <pawlows.filip@gmail.com>
2023-04-06 11:36:00 +0200

    Create main.py for running the bot

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import bot
-import discord
-from discord.ext import commands
-from config import TOKEN
 
 if __name__ == '__main__':
-    # Create bot instance
-    bot_client = commands.Bot(command_prefix="!")
-
-    # Initialize bot
-    bot.initialize(bot_client)
-
     # Run the bot
-    bot_client.run(TOKEN)
+    bot.run_discord_bot()
